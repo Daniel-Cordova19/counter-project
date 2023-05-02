176eda241ad60c4d969bc8c7ab10043a1d613542
Daniel Cordova <javierdanielcordova@gmail.com>
2023-05-02 14:09:09 +0200

    Ejercicio Avanzado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 // Tu código aquí.
+
+// Variable de estado: un number para almacenar el valor del contador actual
+let counter = 0;// Tu código aquí.
+
+// Seleccionar el botón ADD COUNT y añadir un listener para el evento 'click'. Cada vez que el usuario haga click en el botón, incrementamos en 1 la variable de estado. 
+
+document.querySelector(".nextBtn").addEventListener("click", () => {
+    counter++;
+    document.querySelector("#counter").innerHTML = counter;
+    counter.style.backgroundColor = 'red';
+
+    counter.addEventListener('click', function() {
+        caja.style.backgroundColor = 'red';
+      });
+});
+
+
+
+  document.querySelector(".prevBtn").addEventListener("click", () => {
+    counter--;
+    document.querySelector("#counter").innerHTML = counter;
+  });
